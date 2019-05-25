77b9932dc7ba1271a7a120c9c4e9dd16dc8d18cb
AbelAvalos16 <abel8avalos@gmail.com>
2019-05-25 15:57:13 -0400

    Creación de la ventana para la historia 5

---
+++ ProyectoNomina/ProyectoNomina/w_adelantosPedidos.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoNomina
+{
+    /// <summary>
+    /// Lógica de interacción para w_adelantosPedidos.xaml
+    /// </summary>
+    public partial class w_adelantosPedidos : Window
+    {
+        ConexionDB datos;
+        public w_adelantosPedidos()
+        {
+            InitializeComponent();
+            datos = new ConexionDB();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cargardatos();
+        }
+
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+            cargardatos();
+        }
+
+        void cargardatos()
+        {
+            dgSolicitudes.ItemsSource = null;
+            var solicitudes = datos.Anticipoes.ToList();
+            dgSolicitudes.ItemsSource = solicitudes;
+        }
+    }
+}
