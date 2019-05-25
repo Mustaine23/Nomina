46ad63a9fdd55f316c3d6051f8c33ef5a1fd77dc
gabososa <gabtonysosa@hotmail.com>
2019-05-25 15:47:41 -0400

    Se creó el formulario para la historia 4

---
+++ ProyectoNomina/ProyectoNomina/w_concepto.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoNomina
+{
+    /// <summary>
+    /// Lógica de interacción para w_concepto.xaml
+    /// </summary>
+    public partial class w_concepto : Window
+    {
+        ConexionDB datos;
+        public w_concepto()
+        {
+            InitializeComponent();
+            datos = new ConexionDB();
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Concepto em = new Concepto();
+                em.Descripcion = txtDescripcion.Text.Trim();
+                em.Tipo = txtTipo.Text.Trim();
+                
+
+                datos.Conceptoes.Add(em);
+                datos.SaveChanges();
+
+                MessageBox.Show("Concepto insertado correctamente.", "PROCESO FINALIZADO", MessageBoxButton.OK, MessageBoxImage.Information);
+                LimpiarCampos();
+    }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió el siguiente error: " + ex.ToString(), "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+         void LimpiarCampos()
+        {
+            txtDescripcion.Text = string.Empty;
+            txtTipo.Text = string.Empty;
+          
+            actualizarGrilla();
+            txtDescripcion.Focus();
+        }
+        void actualizarGrilla()
+        {
+            dgConceptos.ItemsSource = null;
+            var dtConcepto = datos.Conceptoes.ToList();
+            dgConceptos.ItemsSource = dtConcepto;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+    }
+}
