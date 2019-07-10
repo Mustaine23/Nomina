73540c6c66134ccbeac98223e360b713a1605dc5
AbelAvalos16 <abelavalos@gmail.com>
2019-07-09 23:21:26 -0400

    desarrollo del proceso de liquidacion

--- ProyectoNomina/ProyectoNomina/w_ProcesoLiquidacion.xaml.cs
+++ ProyectoNomina/ProyectoNomina/w_ProcesoLiquidacion.xaml.cs
@@ -52,7 +52,7 @@ namespace ProyectoNomina
                         int vIngreso = 0;
                         int vEgresos = 0;
                         int vTotal = 0;
-
+                        double vIps = 0;
                         var detalleLiquidacion = ee.Liquidacion_Mensual_Detalle.ToList().FindAll(x => x.Liquidacion_Id == dd.Id_Liquidacion);
 
                         if (detalleLiquidacion != null)
@@ -70,17 +70,39 @@ namespace ProyectoNomina
                             }
                         }
 
+                        vIps = vIngreso * 0.09;
+                        Liquidacion_Mensual_Detalle nuevoIps = new Liquidacion_Mensual_Detalle();
+                        nuevoIps.Concepto_Id = 1;
+                        nuevoIps.Empleado = ee;
+                        nuevoIps.Liquidacion_Mensual = dd;
+                        nuevoIps.Monto = int.Parse((vIps * -1).ToString());
+
+                        datos.Liquidacion_Mensual_Detalle.Add(nuevoIps);
+                        datos.SaveChanges();
+
                         var adelantos = ee.Anticipo.ToList().FindAll(x => DateTime.Parse(x.Fecha_Definicion.ToString()).Month == dd.Mes && DateTime.Parse(x.Fecha_Definicion.ToString()).Year == dd.Anho && x.Estado == "A");
 
                         if (adelantos != null)
                         {
+                            int vAnticipo = 0;
                             foreach (Anticipo an in adelantos)
                             {
                                 vEgresos = vEgresos + an.Monto_Aprobado;
+                                vAnticipo = vAnticipo + an.Monto_Aprobado;
                             }
-                        }
-                    }
 
+                            Liquidacion_Mensual_Detalle anticipo = new Liquidacion_Mensual_Detalle();
+                            anticipo.Concepto_Id = 2;
+                            anticipo.Empleado = ee;
+                            anticipo.Liquidacion_Mensual = dd;
+                            anticipo.Monto = vAnticipo * -1;
+
+                            datos.Liquidacion_Mensual_Detalle.Add(anticipo);
+                            datos.SaveChanges();
+                        }
+
+                        vTotal = vIngreso - vEgresos;
+                    }
                 }
                 else
                 {
